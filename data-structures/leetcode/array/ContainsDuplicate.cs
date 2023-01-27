ee220315de2663018872112b14f4e0fc4e72379f
Oleh Strokan <oleh14ua71@gmail.com>
2023-01-27 15:18:43 +0100

    leetcode: contains duplicate, missing number update

--- data-structures/leetcode/array/ContainsDuplicate.cs
+++ data-structures/leetcode/array/ContainsDuplicate.cs
@@ -2,16 +2,28 @@ namespace data_structures.leetcode.array;
 
 public static class ContainsDuplicate
 {
-    public static bool IsContaining(int[] numbers)
+    public static bool IsContainingByList(int[] nums)
     {
         List<int> list = new List<int>();
-        foreach (int i in numbers)
+        foreach (int i in nums)
         {
             if (list.Contains(i))
             {
                 return true;
             }
             list.Add(i);
+        }
+        return false;
+    }
+
+    public static bool IsContaining(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
         }
         return false;
     }
--- data-structures/leetcode/array/MissingNumber.cs
+++ data-structures/leetcode/array/MissingNumber.cs
@@ -4,7 +4,7 @@ public static class MissingNumber
 {
     public static int ReturnMissingValue(int[] nums)
     {
-        List<int> hash = new List<int>(nums);
+        HashSet<int> hash = new HashSet<int>(nums);
 
         for (int i = 0; i < nums.Length; i++) {
             if (!hash.Contains(i)) return i;
